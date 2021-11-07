59cb7b3b31831cf3cea5ec908db49580f1d62959
drish-01-ya <70087742+drish-01-ya@users.noreply.github.com>
2021-11-07 11:40:28 +0530

    changes

--- home/login.js
+++ home/login.js
@@ -2,11 +2,28 @@ let email = document.getElementById("email");
 let pwd = document.getElementById("pwd");
 
 function validate(){
-    if(email.value==""|| pwd.value==""){
-        alert("Fields cannot be empty");
-        return true;
-    }
-    else
-        //alert("Validation is proper");
+    // if(email.value==""|| pwd.value==""){
+    //     alert("Fields cannot be empty");
+    //     return true;
+    // }
+    // else
+    //     //alert("Validation is proper");
+    //     return false;
+
+
+    if(email.value==""){
+        alert("Email cannot be empty");
         return false;
+    }
+    else if(pwd.value==""){
+        alert("Password cannot be blank");
+        return false;
+    }
+    else if(pwd.value.length<=8){
+        alert("Password is too short");
+        pwd.style.border="2px solid red";
+    }
+    else return true;
 }
+
+
