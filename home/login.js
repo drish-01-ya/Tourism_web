a05ac645fffe8d50456f48d1ba9c5c003c882221
drish-01-ya <70087742+drish-01-ya@users.noreply.github.com>
2021-11-07 11:31:57 +0530

    changes

--- home/login.js
+++ home/login.js
@@ -1,2 +1,12 @@
 let email = document.getElementById("email");
 let pwd = document.getElementById("pwd");
+
+function validate(){
+    if(email.value==""|| pwd.value==""){
+        alert("Fields cannot be empty");
+        return true;
+    }
+    else
+        //alert("Validation is proper");
+        return false;
+}
